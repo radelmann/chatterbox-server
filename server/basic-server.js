56655f37ab7fe80b9db4faaeb49860bd9cf6e28a
radelmann <robadelmann@gmail.com>
2015-12-15 15:50:40 -0800

    More refactoring

--- server/basic-server.js
+++ server/basic-server.js
@@ -3,10 +3,6 @@ var express = require('express');
 var app = express();
 var handleRequest = require('./request-handler.js');
 
-//var http = require("http");
-
-// route all root dir requests to 
-// ../client/client/
 app.use('/', express.static('../client/client/'));
 
 app.get('/messages', handleRequest);
@@ -18,4 +14,5 @@ var port = 3000;
 var ip = "127.0.0.1";
 
 console.log("Listening on http://" + ip + ":" + port);
+
 app.listen(port, ip);
